82fdaca354e78c5177881ebe59cf4386d2d18914
Simon <zimon@posteo.de>
2023-05-28 14:44:52 +0200

    feat: added rust to lsp setup

--- nvim/lua/user/lsp/configs.lua
+++ nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua","pyright","texlab" }
+local servers = { "jsonls", "sumneko_lua","pyright","texlab", "rust_analyzer" }
 
 lsp_installer.setup {
 	ensure_installed = servers
